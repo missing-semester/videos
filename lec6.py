c0980901641f95589573e956c697ddb52f0747a8
Anish Athalye <me@anishathalye.com>
2020-01-29 23:08:02 -0500

    Synchronize audio/video tracks in lecture 6
    This was done incorrectly for all previous videos. For one, the videos
    were not in sync (the screen recording was out of sync with the rest).
    Also, the audio was synced incorrectly, because it was done while
    wearing Bluetooth headphones, which introduces extra latency.

--- lec6.py
+++ lec6.py
@@ -1,25 +1,17 @@
 from lib import *
 
 part_1 = 'MIT-Missing-Semester-lec-mit-0000-2020jan22-1406-Adhoc Section_{}.mp4'
-audio_1 = part_1.format(1)
-screen_1_f = part_1.format(2)
-chalkboard_1_f = part_1.format(3)
-wide_1_f = part_1.format(4)
+audio_1 = Audio(part_1.format(1), volume=10.0, delay=3/30)
+screen_only_1 = Fullscreen(part_1.format(2), delay=5/30)
+chalkboard_1 = Fullscreen(part_1.format(3))
+wide_1 = Fullscreen(part_1.format(4))
+screen_1 = Overlay(screen_only_1, wide_1, crop_x=1243, crop_y=424, crop_width=470, opacity=0.85)
 
 part_2 = 'MIT-Missing-Semester-lec-mit-0000-2020jan23-1313-Adhoc Section_{}.mp4'
-audio_2 = part_2.format(1)
-screen_2_f = part_2.format(2)
-wide_2_f = part_2.format(4)
-
-wide_1 = Fullscreen(wide_1_f)
-chalkboard_1 = Fullscreen(chalkboard_1_f)
-screen_1 = Overlay(screen_1_f, wide_1_f, crop_x=1243, crop_y=424, crop_width=470, opacity=0.85)
-
-wide_2 = Fullscreen(wide_2_f)
-screen_2 = Overlay(screen_2_f, wide_2_f, crop_x=1256, crop_y=443, crop_width=431, opacity=0.85)
-
-audio_volume = 10.0
-audio_offset = -0.06
+audio_2 = Audio(part_2.format(1), volume=10.0, delay=7/30)
+screen_only_2 = Fullscreen(part_2.format(2), delay=6/30)
+wide_2 = Fullscreen(part_2.format(4))
+screen_2 = Overlay(screen_only_2, wide_2, crop_x=1256, crop_y=443, crop_width=431, opacity=0.85)
 
 Playlist([
   Multitrack([
@@ -40,8 +32,8 @@ Playlist([
     Clip(wide_1, end='46:39'),
     Clip(screen_1, end='47:05'),
     Clip(wide_1, end='47:44'),
-  ], audio_1, audio_volume, audio_offset),
+  ], audio_1),
   Multitrack([
     Clip(screen_2, start='0:27', end='38:20')
-  ], audio_2, audio_volume, audio_offset),
+  ], audio_2),
 ]).render('lec6.mp4')
