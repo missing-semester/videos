7045421e2bbd7bfd1f1ff820154733dc530149f0
Anish Athalye <me@anishathalye.com>
2020-01-31 11:58:19 -0500

    Sync audio/video in lecture 3

--- lec3.py
+++ lec3.py
@@ -1,18 +1,11 @@
 from lib import *
 
-audio = 'MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_1.mp4'
-tracking_f = 'MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_1.mp4'
-screen_f = 'MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_2.mp4'
-chalkboard_f = 'MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_3.mp4'
-wide_f = 'MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_4.mp4'
-
-wide = Fullscreen(wide_f)
-tracking = Fullscreen(tracking_f)
-chalkboard = Fullscreen(chalkboard_f)
-screen = Overlay(screen_f, wide_f, crop_x=1458, crop_y=394, crop_width=462, opacity=0.85)
-
-audio_volume = 10.0
-audio_offset = -0.06
+audio = Audio('MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_1.mp4', delay=6/30)
+tracking = Fullscreen('MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_1.mp4', delay=-1/30)
+screen_only = Fullscreen('MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_2.mp4', delay=5/30)
+chalkboard = Fullscreen('MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_3.mp4')
+wide = Fullscreen('MIT-Missing-Semester-lec-mit-0000-2020jan15-1406-Adhoc Section_4.mp4')
+screen = Overlay(screen_only, wide, crop_x=1458, crop_y=394, crop_width=462, opacity=0.85)
 
 Multitrack([
   Clip(wide, start='0:38', end='5:25'), # tracking cam isn't locked in at start
@@ -22,4 +15,4 @@ Multitrack([
   Clip(tracking, end='21:00'),
   Clip(screen, end='48:00'),
   Clip(wide, end='49:04'),
-], audio, audio_volume, audio_offset).render('lec3.mp4')
+], audio).render('lec3.mp4')
