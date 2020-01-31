a868c11c12609fd24ce813a9c4a8f71d03bebdb7
Anish Athalye <me@anishathalye.com>
2020-01-31 11:38:52 -0500

    Sync audio/video in lecture 2

--- lec2.py
+++ lec2.py
@@ -1,19 +1,14 @@
 from lib import *
 from typing import Dict, Any
 
-audio = 'MIT-Missing-Semester-lec-mit-0000-2020jan14-1400-Adhoc Section_1.mp4'
-tracking = 'MIT-Missing-Semester-lec-mit-0000-2020jan14-1400-Adhoc Section_1.mp4'
-screen = 'MIT-Missing-Semester-lec-mit-0000-2020jan14-1400-Adhoc Section_2.mp4'
-chalkboard = 'MIT-Missing-Semester-lec-mit-0000-2020jan14-1400-Adhoc Section_3.mp4'
-wide = 'MIT-Missing-Semester-lec-mit-0000-2020jan14-1400-Adhoc Section_4.mp4'
-
-audio_volume = 10.0
-audio_offset = -0.06
-overlay_params: Dict[str, Any] = {'crop_x': 1466, 'crop_y': 458, 'crop_width': 454, 'opacity': 0.85}
+audio = Audio('MIT-Missing-Semester-lec-mit-0000-2020jan14-1400-Adhoc Section_1.mp4', delay=6/30)
+screen_only = Fullscreen('MIT-Missing-Semester-lec-mit-0000-2020jan14-1400-Adhoc Section_2.mp4', delay=6/30)
+wide = Fullscreen('MIT-Missing-Semester-lec-mit-0000-2020jan14-1400-Adhoc Section_4.mp4')
+screen = Overlay(screen_only, wide, crop_x=1466, crop_y=458, crop_width=454, opacity=0.85)
 
 Multitrack([
-  Clip(Fullscreen(wide), start='04:40', end='05:40'),
-  Clip(Overlay(screen, wide, **overlay_params), end='51:55'),
-  Clip(Fullscreen(wide), end='53:35')
-], audio, audio_volume, audio_offset).render('lec2.mp4')
+  Clip(wide, start='04:40', end='05:40'),
+  Clip(screen, end='51:55'),
+  Clip(wide, end='53:35')
+], audio).render('lec2.mp4')
 
