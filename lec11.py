3c1a237dda02f494a17a9b37c3eeb6fe1c8101eb
Anish Athalye <me@anishathalye.com>
2020-02-02 14:39:58 -0500

    Tweak timing

--- lec11.py
+++ lec11.py
@@ -6,5 +6,5 @@ wide = Fullscreen('MIT-Missing-Semester-lec-mit-0000-2020jan30-1405-Adhoc Sectio
 main = Overlay(wide, screen, crop_x=0, crop_y=78, crop_width=1920, crop_height=234, width=1920, margin=0, opacity=1, location=Location.TOP_CENTER)
 
 Multitrack([
-  Clip(main, start='1:50', end='55:39'),
+  Clip(main, start='1:50', end='55:42'),
 ], audio).render('lec11.mp4')
