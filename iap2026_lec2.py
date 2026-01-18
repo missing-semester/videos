ff02d1c218126d3987183ade7d75dde00d9089a2
Anish Athalye <me@anishathalye.com>
2026-01-17 21:56:31 -0500

    Add lecture 2 title and description

--- iap2026_lec2.py
+++ iap2026_lec2.py
@@ -22,3 +22,24 @@ Multitrack(
     ],
     audio,
 ).render("lec2.mp4", title="Missing Semester IAP 2026: Lecture 2")
+
+"""
+Title: Lecture 2: Command-line Environment
+
+Description:
+
+You can find the lecture notes and exercises for this lecture at https://missing.csail.mit.edu/2026/command-line-environment/
+
+0:00 - Introduction
+1:17 - The Command Line Interface
+2:37 - Arguments
+10:26 - Streams
+16:19 - Environment variables
+21:08 - Return codes
+26:04 - Signals
+31:59 - Remote Machines
+37:30 - Terminal Multiplexers
+41:24 - Customizing the Shell
+1:00:28 - AI in the Shell
+1:04:56 - Terminal Emulators
+"""
