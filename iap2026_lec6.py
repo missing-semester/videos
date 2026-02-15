cfd6e1cfe21bc4e3d5cdc85b728b12e55d2ea9bb
Anish Athalye <me@anishathalye.com>
2026-02-15 11:27:51 -0800

    Add frame rate conversion

--- iap2026_lec6.py
+++ iap2026_lec6.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from lib import Audio, Clip, Fullscreen, Multitrack, Overlay
+from lib import Audio, Clip, Framerate, Fullscreen, Multitrack, Overlay
 
 os.chdir(sys.argv[1])
 
@@ -9,7 +9,7 @@ os.chdir(sys.argv[1])
 # noise source is lecture 6, approx 8:03.70 -- 8:04.85; this only has background static
 # noise reduction applied with settings: reduction 30 dB, sensitivity 6.00, frequency smoothing 2 bands
 audio = Audio("denoised.wav", delay=0.12)
-tracking = Fullscreen("Missing-Semester-of-your-CS-Education-on-1_20_2026-(Tue)-Tracking-2026jan20.mp4")
+tracking = Framerate(Fullscreen("Missing-Semester-of-your-CS-Education-on-1_20_2026-(Tue)-Tracking-2026jan20.mp4"), 30)
 screen_only = Fullscreen("Missing-Semester-of-your-CS-Education-on-1_20_2026-(Tue)-PC2-2026jan20.mp4", delay=-3 / 60)
 
 screen_lectern = Overlay(screen_only, tracking, crop_x=680, crop_y=260, crop_width=600, crop_height=440, opacity=0.85)
