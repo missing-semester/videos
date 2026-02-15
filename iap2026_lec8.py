a9acd12228474b8910c342623b61a91620a1af4a
Anish Athalye <me@anishathalye.com>
2026-02-15 08:33:14 -0800

    Add script for lecture 8

---
+++ iap2026_lec8.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+from lib import Audio, Clip, Crop, Fullscreen, Multitrack
+
+os.chdir(sys.argv[1])
+
+# noise reduction using Audacity,
+# noise source is lecture 8, approx 24:46.75 -- 24:48.00; works better than using lecture 3's noise sample
+# noise reduction applied with settings: reduction 30 dB, sensitivity 6.00, frequency smoothing 2 bands
+audio = Audio("denoised.wav", delay=0.14)
+tracking = Fullscreen("Missing-Semester-of-your-CS-Education-on-1_22_2026-(Thu)-Tracking-2026jan22.mp4")
+wide = Fullscreen("Missing-Semester-of-your-CS-Education-on-1_22_2026-(Thu)-Wideshot-2026jan22.mp4", delay=30 / 60)
+
+wide_crop = Crop(wide, x=840, y=350, width=800)
+
+Multitrack(
+    [
+        Clip(wide_crop, start="4:21", end="1:09:58"),
+    ],
+    audio,
+).render("lec8.mp4", title="Missing Semester IAP 2026: Lecture 8")
