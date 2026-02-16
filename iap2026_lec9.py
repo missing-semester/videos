d9315a88ff41205b0b3668ee186f8db2a35c1e19
Anish Athalye <me@anishathalye.com>
2026-02-16 08:45:14 -0800

    Add lecture 9 title and description

--- iap2026_lec9.py
+++ iap2026_lec9.py
@@ -22,3 +22,21 @@ Multitrack(
     ],
     audio,
 ).render("lec9.mp4", title="Missing Semester IAP 2026: Lecture 9")
+
+"""
+Title: Lecture 9: Code Quality
+
+Description:
+
+You can find the lecture notes and exercises for this lecture at https://missing.csail.mit.edu/2026/code-quality/
+
+0:00:00 - Introduction
+0:01:28 - Formatting
+0:07:07 - Linting
+0:20:42 - Testing
+0:39:57 - Pre-commit hooks
+0:42:02 - Continuous integration
+0:49:08 - Command runners
+0:51:07 - Regular expressions
+1:06:11 - Conclusion and Q&A
+"""
